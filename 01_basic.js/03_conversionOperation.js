3d3339504744c8147719ec2a941fb7bb743e0373
Vikas maurya <vikasmaurya9506@gmail.com>
2026-04-03 16:51:26 +0000

    conversion of data types

---
+++ 01_basic.js/03_conversionOperation.js
@@ -0,0 +1,34 @@
+// let score=33 //number
+//let score="vikas"  //string 
+
+
+//console.log(typeof (score));
+
+//let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+//"333abc" => NaN
+//true =>1 ; false=>0
+
+
+//let isLoggedIn=1;
+
+//let booleanIsLoggedIn=boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1=> true; 0==> false
+//"" => false  //empty string
+//"vikas" => true
+
+let someNumber=33     //dekhne me number hai but ham isae string me convert kar diye hai 
+
+let stringNumber=String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+ 
